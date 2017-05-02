df4b7f61339d23ef24fd3118e251e40a8c7bb094
M.Gerald Junhakristama <geraldjunha@gmail.com>
2017-05-02 11:42:33 +0700

    Akhir

---
+++ Asp_mvc_2/Security/AuthorizeRoleAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Asp_mvc_2.Models.DB;
+using Asp_mvc_2.Models.EntityManager;
+using Asp_mvc_2.Security;
+
+namespace Asp_mvc_2.Security
+{
+    public class AuthorizeRoleAttribute : AuthorizeAttribute
+    {
+        private readonly string[] userAssignedRoles;
+        public AuthorizeRoleAttribute(params string[] roles)
+        {
+            this.userAssignedRoles = roles;
+        }
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            bool authorize = false;
+            using (Demo1DBEntitiesEntities db = new Demo1DBEntitiesEntities())
+            {
+                UserManager UM = new UserManager();
+                foreach (var roles in userAssignedRoles)
+                {
+                    authorize = UM.IsUserInRole(httpContext.User.Identity.Name, roles);
+                    if (authorize)
+                        return authorize;
+                }
+            }
+            return authorize;
+        }
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("~/Home/UnAuthorized");
+        }
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,80 @@
+﻿CREATE TABLE [dbo].[LOOKUPRole](   
+    [LOOKUPRoleID] [int] IDENTITY(1,1) NOT NULL, 
+    [RoleName] [varchar](100) DEFAULT '', 
+    [RoleDescription] [varchar](500) DEFAULT '', 
+    [RowCreatedSYSUserID] [int] NOT NULL, 
+    [RowCreatedDateTime] [datetime]  DEFAULT GETDATE(), 
+    [RowModifiedSYSUserID] [int] NOT NULL, 
+    [RowModifiedDateTime] [datetime] DEFAULT GETDATE(), 
+PRIMARY KEY (LOOKUPRoleID)   
+    ) 
+GO   
+
+INSERT INTO LOOKUPRole (RoleName,RoleDescription,RowCreatedSYSUserID,RowModifiedSYSUserID)   
+       VALUES ('Admin','Can Edit, Update, Delete',1,1) 
+INSERT INTO LOOKUPRole (RoleName,RoleDescription,RowCreatedSYSUserID,RowModifiedSYSUserID)   
+       VALUES ('Member','Read only',1,1)   
+GO 
+
+CREATE TABLE [dbo].[SYSUser](   
+    [SYSUserID] [int] IDENTITY(1,1) NOT NULL, 
+    [LoginName] [varchar](50) NOT NULL, 
+    [PasswordEncryptedText] [varchar](200) NOT NULL, 
+    [RowCreatedSYSUserID] [int] NOT NULL, 
+    [RowCreatedDateTime] [datetime] DEFAULT GETDATE(), 
+    [RowModifiedSYSUserID] [int] NOT NULL, 
+    [RowMOdifiedDateTime] [datetime] DEFAULT GETDATE(), 
+    PRIMARY KEY (SYSUserID) 
+) 
+GO   
+
+CREATE TABLE [dbo].[SYSUserProfile](   
+    [SYSUserProfileID] [int] IDENTITY(1,1) NOT NULL, 
+    [SYSUserID] [int] NOT NULL, 
+    [FirstName] [varchar](50) NOT NULL, 
+    [LastName] [varchar](50) NOT NULL, 
+    [Gender] [char](1) NOT NULL, 
+    [RowCreatedSYSUserID] [int] NOT NULL, 
+    [RowCreatedDateTime] [datetime] DEFAULT GETDATE(), 
+    [RowModifiedSYSUserID] [int] NOT NULL, 
+    [RowModifiedDateTime] [datetime] DEFAULT GETDATE(), 
+    PRIMARY KEY (SYSUserProfileID) 
+    ) 
+GO 
+ 
+ALTER TABLE [dbo].[SYSUserProfile]  WITH CHECK ADD FOREIGN KEY([SYSUserID])   
+REFERENCES [dbo].[SYSUser] ([SYSUserID])   
+GO   
+
+
+CREATE TABLE [dbo].[SYSUserRole](   
+    [SYSUserRoleID] [int] IDENTITY(1,1) NOT NULL, 
+    [SYSUserID] [int] NOT NULL, 
+    [LOOKUPRoleID] [int] NOT NULL, 
+    [IsActive] [bit] DEFAULT (1), 
+    [RowCreatedSYSUserID] [int] NOT NULL, 
+    [RowCreatedDateTime] [datetime] DEFAULT GETDATE(), 
+    [RowModifiedSYSUserID] [int] NOT NULL, 
+    [RowModifiedDateTime] [datetime] DEFAULT GETDATE(), 
+    PRIMARY KEY (SYSUserRoleID) 
+) 
+GO 
+ 
+ALTER TABLE [dbo].[SYSUserRole]  WITH CHECK ADD FOREIGN KEY([LOOKUPRoleID])   
+REFERENCES [dbo].[LOOKUPRole] ([LOOKUPRoleID])   
+GO 
+ 
+ALTER TABLE [dbo].[SYSUserRole]  WITH CHECK ADD FOREIGN KEY([SYSUserID])   
+REFERENCES [dbo].[SYSUser] ([SYSUserID])   
+GO
+
+INSERT INTO SYSUser (LoginName,PasswordEncryptedText, RowCreatedSYSUserID, RowModifiedSYSUserID)   
+VALUES ('Admin','Admin',1,1)   
+GO 
+  
+INSERT INTO SYSUserProfile (SYSUserID,FirstName,LastName,Gender,RowCreatedSYSUserID, RowModifiedSYSUserID)   
+VALUES (2,'Vinz','Durano','M',1,1)   
+GO 
+ 
+INSERT INTO SYSUserRole (SYSUserID,LOOKUPRoleID,IsActive,RowCreatedSYSUserID, RowModifiedSYSUserID)   
+VALUES (2,1,1,1,1)
